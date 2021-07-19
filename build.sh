366e249b852e6c66b8d627e40760dd15fb88bceb
Hezi Shahmoon <hshahmoon@apple.com>
2021-07-19 23:32:12 +0300

    Locate antlr jar according to symlink
    Signed-off-by: Hezi Shahmoon <hshahmoon@apple.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -R scad
 antlr scad.g4 -o scad
 pushd $(pwd)
 cd scad
-javac -classpath /opt/brew//Cellar/antlr/4.9.1/antlr-4.9.1-complete.jar scad*.java
+javac -classpath $(which antlr| echo $(dirname $(which antlr))/$(dirname $(xargs readlink))/../antlr-4.9.2-complete.jar) scad*.java
 #echo "module hezi () \n { \n }" | grun scad prog -gui
 grun scad prog -gui < ../DoorStop.scad
 popd
